76a1667f4bf37bb7ea3b99bfd85bc79314d56df1
unknown <dzhani.ivanov@gmail.com>
2024-03-14 16:30:24 +0200

    dynamic header added

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,5 +1,11 @@
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "@/constants";
+
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return (
+    <Header title={transformation.title} subtitle={transformation.subtitle} />
+  );
 };
 
 export default AddTransformationTypePage;
