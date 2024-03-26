9a7c7e6fff418e922391c6736be93b0a689e1d7b
unknown <dzhani.ivanov@gmail.com>
2024-03-26 11:31:42 +0200

    stripe integrated,profile page

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -138,7 +138,37 @@ export async function getAllImages({
     return {
       data: JSON.parse(JSON.stringify(images)),
       totalPage: Math.ceil(totalImages / limit),
-      savedImages
+      savedImages,
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function getUserImages({
+  limit = 9,
+  page = 1,
+  userId,
+}: {
+  limit?: number;
+  page: number;
+  userId: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find({ author: userId }))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const totalImages = await Image.find({ author: userId }).countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPages: Math.ceil(totalImages / limit),
     };
   } catch (error) {
     handleError(error);
